6d636360fbdcd66d721b16727b2898bc92263910
Jeremy <senn.jeremy@gmail.com>
2020-09-22 07:05:45 -0700

    Allow for searching over transactions by common attributes

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -8,6 +8,13 @@ class Api::V1::TransactionsController < ApplicationController
     .start_time(parse_datetime(params[:start_time]))
     .end_time(parse_datetime(params[:end_time]))
     .from_customer_id(params[:FromCustID])
+    .to_customer_id(params[:ToCustID])
+    .from_account_id(params[:from_acct_id])
+    .to_account_id(params[:to_acct_id])
+    .transaction_code(params[:tran_code])
+    .secondary_transaction_code(params[:sec_tran_code])
+    .error_code(params[:error_code])
+    .original_transaction_id(params[:OrigTranID])
     .limit(transactions_limit)
 #    render json: @transactions
     render json: JSON.pretty_generate(@transactions.as_json)
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,6 +6,13 @@ class Transaction < ApplicationRecord
   
   scope :device, ->(dev_id) { where("dev_id = ?", dev_id) unless dev_id.blank?}
   scope :from_customer_id, ->(fromcustid) { where("FromCustID = ?", fromcustid) unless fromcustid.blank?}
+  scope :to_customer_id, ->(tocustid) { where("ToCustID = ?", tocustid) unless tocustid.blank?}
+  scope :from_account_id, ->(from_acct_id) { where("from_acct_id = ?", from_acct_id) unless from_acct_id.blank?}
+  scope :to_account_id, ->(to_acct_id) { where("to_acct_id = ?", to_acct_id) unless to_acct_id.blank?}
+  scope :transaction_code, ->(tran_code) { where("tran_code = ?", tran_code) unless tran_code.blank?}
+  scope :secondary_transaction_code, ->(sec_tran_code) { where("sec_tran_code = ?", sec_tran_code) unless sec_tran_code.blank?}
+  scope :error_code, ->(error_code) { where("error_code = ?", error_code) unless error_code.blank?}
+  scope :original_transaction_id, ->(orig_tran_id) { where("OrigTranID = ?", orig_tran_id) unless orig_tran_id.blank?}
   scope :start_time, ->(start_time) { where('date_time >= ?', start_time.to_datetime) unless start_time.blank?}
   scope :end_time, ->(end_time) { where('date_time <= ?', end_time.to_datetime) unless end_time.blank?}
   
