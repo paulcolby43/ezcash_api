8f64808347b137a64ad64241d29d8bc04f6e1798
Jeremy <senn.jeremy@gmail.com>
2021-06-15 07:57:07 -0700

    Allow for posting to denoms

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       end
       resources :bill_counts
       resources :bill_hists
-      resources :denoms, only: [:show, :index]
+      resources :denoms
       resources :transactions
       resources :customer_barcodes do
         collection do
