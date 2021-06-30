43b0a6c475ab2ed861201cf40941fbf6e95228c6
Jeremy <senn.jeremy@gmail.com>
2021-06-30 11:05:26 -0700

    Bill_hists optional belongs_to device and company

--- app/models/bill_hist.rb
+++ app/models/bill_hist.rb
@@ -3,8 +3,8 @@ class BillHist < ApplicationRecord
 #  self.primary_keys = :cassette_id, :dev_id #Composite primary keys
   self.table_name= 'bill_hist'
   
-  belongs_to :device, :foreign_key => 'dev_id'
-  belongs_to :company
+  belongs_to :device, :foreign_key => 'dev_id', optional: true
+  belongs_to :company, optional: true
   
   scope :denomination, ->(denomination) { where("denomination = ?", denomination) unless denomination.blank?}
   scope :device, ->(dev_id) { where(dev_id: dev_id) unless dev_id.blank?}
